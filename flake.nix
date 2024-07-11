b385e01229bd359a43ce9a534acbe3e337b78f7d
matthewcroughan <matt@croughan.sh>
2024-07-11 05:24:12 +0100

    try disko

--- flake.nix
+++ flake.nix
@@ -1,8 +1,23 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-  inputs.nixos-hardware.url = "github:nixos/nixos-hardware";
-  outputs = { self, nixpkgs, nixos-hardware }: rec {
-    images.timbo-image = nixosConfigurations.timbo.config.system.build.image;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    disko.url = "github:nix-community/disko";
+    disko-utils.url = "github:matthewcroughan/disko-utils";
+  };
+
+  outputs = { self, nixpkgs, nixos-hardware, disko-utils, disko }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.autoInstaller =
+      (pkgs.writeShellScriptBin "diskoScript" ''
+        set -e
+        PATH=${pkgs.nix}/bin:$PATH
+        ${self.nixosConfigurations.timbo.config.system.build.diskoScript}
+        ${pkgs.nixos-install-tools}/bin/nixos-install --no-root-password --option substituters "" --no-channel-copy --system ${self.nixosConfigurations.timbo.config.system.build.toplevel}
+        ${pkgs.coreutils}/bin/cp --no-preserve=mode -rT ${self} /mnt/etc/nixos
+        ${pkgs.umount}/bin/umount /mnt/boot /mnt
+      '');
     nixosConfigurations.timbo = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -12,8 +27,10 @@
         nixos-hardware.nixosModules.common-cpu-amd-pstate
         nixos-hardware.nixosModules.common-gpu-nvidia
         nixos-hardware.nixosModules.common-gpu-amd
+        disko.nixosModules.disko
         ./configuration.nix
-        ./repart.nix
+        ./disko.nix
+#        ./repart.nix
       ];
     };
   };
